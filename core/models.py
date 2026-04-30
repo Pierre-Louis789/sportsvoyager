66a6fd266161b9ccecf4873b1e90bd0fc2133a3c
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-04-30 16:40:25 +0100

    Add Pack model, migrations, and admin registration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Pack(models.Model):
+    title = models.CharField(max_length=200)
+    club = models.CharField(max_length=200)
+    league = models.CharField(max_length=200, blank=True)
+    description = models.TextField()
+    itinerary = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='packs/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
