2d5e0eb7b2efacf9abcdfe3246dd5235365eadae
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-05-04 10:07:20 +0100

    Add affiliate link fields and booking buttons to pack detail page

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Pack
 
-admin.site.register(Pack)
+class PackAdmin(admin.ModelAdmin):
+    list_display = ('title', 'club', 'league', 'price', 'created_at')
+    search_fields = ('title', 'club', 'league')
+    list_filter = ('league',)
+    fields = (
+        'title', 'club', 'league', 'description', 'itinerary', 'price', 'image',
+        'flight_link', 'hotel_link', 'ticket_link', 'tour_link'
+    )
+
+admin.site.register(Pack, PackAdmin)
--- core/models.py
+++ core/models.py
@@ -8,7 +8,14 @@ class Pack(models.Model):
     itinerary = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='packs/', blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    
+   
+    flight_link = models.URLField(blank=True)
+    hotel_link = models.URLField(blank=True)
+    ticket_link = models.URLField(blank=True)
+    tour_link = models.URLField(blank=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.title
