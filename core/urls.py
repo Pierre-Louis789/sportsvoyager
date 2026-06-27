324349f9d98a09cf12ad317875a0b74089aecac0
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-06-27 13:25:36 +0100

    fix core urls.py login authentication form

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
 
     # AUTH
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='auth/login.html',
+            authentication_form=AuthenticationForm
+        ),
+        name='login'
+    ),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('register/', views.register, name='register'),
 
